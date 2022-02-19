15fbe0c7e0376d423388f42fd87d6416653d3a83
Mohammad Daoud <m.a.daoud99@gmail.com>
2022-02-19 19:55:52 +0200

    modify HorizontalScaling to do every minute

--- MasterNode/src/main/java/com/mohammad/masternode/events/HorizontalScaling.java
+++ MasterNode/src/main/java/com/mohammad/masternode/events/HorizontalScaling.java
@@ -5,17 +5,19 @@ import com.mohammad.masternode.cluster.MasterNode;
 import com.mohammad.masternode.cluster.Replica;
 import com.mohammad.masternode.utils.AppLogger;
 import com.mohammad.masternode.utils.ThreadUtils;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Component
-public class HorizontalScaling {
+public class HorizontalScaling extends TimerTask {
 
     private final AppLogger LOGGER = AppLogger.create("HorizontalScaling logger: ");
     private  int maxThreadNumber = 32;
 
-    @Bean
-    public void doScaling() {
+    public void doScalingHelper() {
      int currentThreadNumber = ThreadUtils.threadCounter();
         if (currentThreadNumber > maxThreadNumber) {
             maxThreadNumber += currentThreadNumber;
@@ -23,5 +25,16 @@ public class HorizontalScaling {
             LOGGER.log("Scaling Active 'added new replica");
             LOGGER.log("Number of Replicas: "+ MasterNode.getInstance().getReplicaGroup());
         }
+    }
+
+    @Override
+    public void run(){
+        doScalingHelper();
+    }
+
+    @Autowired
+    public void doScaling(){
+        Timer timer = new Timer();
+        timer.schedule(new HorizontalScaling(),60000);
     }
 }
